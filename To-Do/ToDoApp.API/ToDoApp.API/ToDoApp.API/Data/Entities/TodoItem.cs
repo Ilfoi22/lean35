bdf05f26b166da240d396d770786d9eb69f1abea
Ilfoi22 <andrii.tsunivskii@gmail.com>
2024-01-22 20:38:14 +0200

    update from 22-01-2024

---
+++ To-Do/ToDoApp.API/ToDoApp.API/ToDoApp.API/Data/Entities/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoApp.API.Data.Entities;
+
+public class TodoItem
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CompletedDate { get; set; }
+}
---
+++ To-Do/ToDoApp.API/ToDoApp.API/ToDoApp.API/Services/TodoItemService.cs
@@ -0,0 +1,51 @@
+﻿using ToDoApp.API.Data.Entities;
+using ToDoApp.API.Repositories.Interfaces;
+using ToDoApp.API.Services.Interfaces;
+
+namespace ToDoApp.API.Services;
+
+public class TodoItemService : ITodoItemService
+{
+    private readonly ITodoItemRepository _todoItemRepository;
+
+    public TodoItemService(ITodoItemRepository todoItemRepository)
+    {
+        _todoItemRepository = todoItemRepository;
+    }
+
+    public async Task<IEnumerable<TodoItem?>> GetTodoItemsAsync(int page, int pageSize)
+    {
+        var result = await _todoItemRepository.GetTodoItemsAsync(page, pageSize);
+        return result;
+    }
+
+    public async Task<TodoItem?> GetTodoItemByIdAsync(Guid id)
+    {
+        var result = await _todoItemRepository.GetTodoItemByIdAsync(id);
+        return result;
+    }
+
+    public async Task<IEnumerable<TodoItem?>> SearchTodoItemsByCategory(string category)
+    {
+        var result = await _todoItemRepository.SearchTodoItemsByCategory(category);
+        return result;
+    }
+
+    public async Task<TodoItem?> AddTodoAsync(TodoItem todo)
+    {
+        var result = await _todoItemRepository.AddTodoAsync(todo);
+        return result;
+    }
+
+    public async Task<TodoItem?> DeleteTodoAsync(Guid id)
+    {
+        var result = await _todoItemRepository.DeleteTodoAsync(id);
+        return result;
+    }
+
+    public async Task<TodoItem?> UpdateTodoAsync(Guid id, TodoItem todo)
+    {
+        var result = await _todoItemRepository.UpdateTodoAsync(id, todo);
+        return result;
+    }
+}
