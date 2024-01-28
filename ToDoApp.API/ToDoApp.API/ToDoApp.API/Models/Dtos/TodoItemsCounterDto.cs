f807660e48e6bed7ac08dadcf9ac7e55834730e7
Andrii Tsunivskii <119940082+Ilfoi22@users.noreply.github.com>
2024-01-28 17:09:21 +0200

    Delete ToDoApp.API/ToDoApp.API directory

--- ToDoApp.API/ToDoApp.API/ToDoApp.API/Models/Dtos/TodoItemsCounterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using ToDoApp.API.Data.Entities;
-
-namespace ToDoApp.API.Models.Dtos;
-
-public class TodoItemsCounterDto
-{
-    public IEnumerable<TodoItem>? Items { get; set; }
-    public int TotalCount { get; set; }
-}
--- ToDoApp.API/ToDoApp.API/ToDoApp.API/Models/Requests/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ToDoApp.API.Models.Requests;
-
-public class LoginRequest
-{
-    public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
-}
